27e8e368665f06181887c5c43f294d28daa26b0b
sonaliaute <sonali.aute@bizsupporta.com>
2025-04-04 11:33:18 +0530

    Initial commit with TestNG script

---
+++ src/test/java/PercyPOC/PercyAppiumTest.java
@@ -0,0 +1,43 @@
+package PercyPOC;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.percy.selenium.Percy;
+ 
+public class PercyAppiumTest {
+    WebDriver driver;
+    Percy percy;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\202503199\\Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        percy = new Percy(driver);
+    }
+
+    @Test
+    public void testVisual() {
+        try {
+            // Open webpage
+            driver.get("https://www.bajajfinserv.in/gold-loan");
+            Thread.sleep(6000);
+
+            percy.snapshot("DesktopPDP");
+
+            driver.get("https://hamaramall.com");
+            Thread.sleep(6000);
+            // Take a Percy snapshot
+            percy.snapshot("Bajaj Hamaramall Homepage");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the browser after the test
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+}
